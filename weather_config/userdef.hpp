0ff5b941a33cf7f58996a97f057642c7ca4c3d73
sarrow104 <sarrow104@gmail.com>
2016-05-03 23:42:16 +0800

    2-D array to 1-D vector
    WeatherLanguage[][2] -> LangInfo_t WeatherLanguage[]
    TemperatureType[][2] -> TemperatureUnit_t TemperatureType[]
    rm length info

--- weather_config/userdef.hpp
+++ weather_config/userdef.hpp
@@ -1,34 +1,47 @@
 #ifndef USERDEF_HPP
 #define USERDEF_HPP
 
-const int LangCount = 19;
-const char *WeatherLanguage[LangCount][2] {
-    { "English", "en" },
-    { "Russian", "ru" },
-    { "Italian", "it" },
-    { "Spanish", "es" },
-    { "Ukrainian", "uk" },
-    { "German", "de" },
-    { "Portuguese", "pt" },
-    { "Romanian", "ro" },
-    { "Polish", "pl" },
-    { "Finnish", "fi" },
-    { "Dutch", "nl" },
-    { "French", "fr" },
-    { "Bulgarian", "bg" },
-    { "Swedish", "sv" },
-    { "Chinese Traditional", "zh_tw" },
-    { "Chinese Simplified", "zh_cn" },
-    { "Turkish", "tr" },
-    { "Croatian", "hr" },
-    { "Catalan", "ca" }
+#ifndef USE_STRUCT
+#define USE_STRUCT
+#endif
+
+struct LangInfo_t {
+    const char * fullName;
+    const char * shortName;
 };
 
-const int TempTypeCount = 3;
-const char *TemperatureType[TempTypeCount][2] {
+LangInfo_t WeatherLanguage[] {
+    { "English",    "en" },
+    { "Russian",    "ru" },
+    { "Italian",    "it" },
+    { "Spanish",    "es" },
+    { "Ukrainian",  "uk" },
+    { "German",     "de" },
+    { "Portuguese", "pt" },
+    { "Romanian",   "ro" },
+    { "Polish",     "pl" },
+    { "Finnish",    "fi" },
+    { "Dutch",      "nl" },
+    { "French",     "fr" },
+    { "Bulgarian",  "bg" },
+    { "Swedish",    "sv" },
+    { "Chinese Traditional", "zh_tw" },
+    { "Chinese Simplified", "zh_cn" },
+    { "Turkish",    "tr" },
+    { "Croatian",   "hr" },
+    { "Catalan",    "ca" }
+};
+
+struct TemperatureUnit_t
+{
+    const char * name;
+    const char * shortName;
+};
+
+TemperatureUnit_t TemperatureType[] {
     { "Fahrenheit", "imperial" },
-    { "Celsius", "metric" },
-    { "Kelvin", "" }
+    { "Celsius",    "metric" },
+    { "Kelvin",     "" }
 };
 
 #endif // USERDEF_HPP
